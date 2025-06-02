6f99948c4a03caa3011c35e003b5678d88d2dac2
Shrey <shreyshukla008@gmail.com>
2025-06-02 13:32:23 +0530

    Implemented env variables for Google service account credentials

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,7 +1,20 @@
 const admin = require("firebase-admin");
 // const serviceAccount = require("../jprep-2025-0ca2cf0f30df.json");
 // const serviceAccount = require("../firebaseService.json");
-const serviceAccount = require("../jprep2025.json");
+//const serviceAccount = require("../jprep2025.json");
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),  // Replace literal \n with actual newlines
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_CERT_URL,
+};
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
--- controllers/questionPaper.js
+++ controllers/questionPaper.js
@@ -5,10 +5,22 @@ const path = require("path");
 const fs = require("fs");
 const QuestionPaper = require("../models/questionPaper");
 const Subject = require("../models/subject");
-
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),  // Replace literal \n with actual newlines
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_CERT_URL,
+};
 
 const auth = new google.auth.GoogleAuth({ 
-  keyFile:  path.join(__dirname, '../jprep2025.json'), // Path to your service account key
+  //keyFile:  path.join(__dirname, '../jprep2025.json'), // Path to your service account key
+  credentials: serviceAccount,
   scopes: ["https://www.googleapis.com/auth/drive"],
 });
 const drive = google.drive({ version: "v3", auth });
